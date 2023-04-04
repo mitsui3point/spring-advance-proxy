70139fe3eb0b1281cc079664df9377b516358b3a
mitsui3point <rladbsgh0331@gmail.com>
2023-04-04 16:18:07 +0900

    feat($test.advisor): update- 예제 코드2 - 직접 만든 포인트컷

--- src/test/java/hello/proxy/advisor/AdvisorTest.java
+++ src/test/java/hello/proxy/advisor/AdvisorTest.java
@@ -4,10 +4,16 @@ import hello.proxy.common.advice.TimeAdvice;
 import hello.proxy.common.service.ServiceImpl;
 import hello.proxy.common.service.ServiceInterface;
 import hello.proxy.log.LogAppenders;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.aop.ClassFilter;
+import org.springframework.aop.MethodMatcher;
 import org.springframework.aop.Pointcut;
 import org.springframework.aop.framework.ProxyFactory;
 import org.springframework.aop.support.DefaultPointcutAdvisor;
+
+import java.lang.reflect.Method;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -62,11 +68,87 @@ public class AdvisorTest extends LogAppenders {
         proxy.save();
         proxy.find();
 
-        assertThat(getContainsLog("TimeProxy 실행")).isPresent();
-        assertThat(getContainsLog("save 호출")).isPresent();
-        assertThat(getContainsLog("TimeProxy 종료 resultTime=")).isPresent();
-        assertThat(getContainsLog("TimeProxy 실행")).isPresent();
-        assertThat(getContainsLog("find 호출")).isPresent();
-        assertThat(getContainsLog("TimeProxy 종료 resultTime=")).isPresent();
+        assertThat(getOrderedLogs().get(0)).contains("TimeProxy 실행");
+        assertThat(getOrderedLogs().get(1)).contains("save 호출");
+        assertThat(getOrderedLogs().get(2)).contains("TimeProxy 종료 resultTime=");
+        assertThat(getOrderedLogs().get(3)).contains("TimeProxy 실행");
+        assertThat(getOrderedLogs().get(4)).contains("find 호출");
+        assertThat(getOrderedLogs().get(5)).contains("TimeProxy 종료 resultTime=");
+    }
+    @Test
+    @DisplayName("직접 만든 포인트컷")
+    void advisorTest2() {
+        ServiceInterface target = new ServiceImpl();
+        ProxyFactory proxyFactory = new ProxyFactory(target);
+        DefaultPointcutAdvisor advisor = new DefaultPointcutAdvisor(new MyPointCut(), new TimeAdvice());
+        proxyFactory.addAdvisor(advisor);
+        ServiceInterface proxy = (ServiceInterface) proxyFactory.getProxy();
+
+        proxy.save();
+        proxy.find();
+
+        assertThat(getOrderedLogs().get(0)).contains("포인트컷 호출 method=save, targetClass=class hello.proxy.common.service.ServiceImpl");
+        assertThat(getOrderedLogs().get(1)).contains("포인트컷 결과=true");
+        assertThat(getOrderedLogs().get(2)).contains("TimeProxy 실행");
+        assertThat(getOrderedLogs().get(3)).contains("save 호출");
+        assertThat(getOrderedLogs().get(4)).contains("TimeProxy 종료 resultTime=");
+
+        assertThat(getOrderedLogs().get(5)).contains("포인트컷 호출 method=find, targetClass=class hello.proxy.common.service.ServiceImpl");
+        assertThat(getOrderedLogs().get(6)).contains("포인트컷 결과=false");
+        assertThat(getOrderedLogs().get(7)).contains("find 호출");
+    }
+
+    /**
+     * 직접 구현한 포인트컷이다. Pointcut 인터페이스를 구현한다.
+     */
+    static class MyPointCut implements Pointcut {
+        @Override
+        public ClassFilter getClassFilter() {
+            return ClassFilter.TRUE;
+        }
+
+        @Override
+        public MethodMatcher getMethodMatcher() {
+            return new MyMethodMatcher();
+        }
+
+    }
+
+    /**
+     * MyMethodMatcher
+     *  직접 구현한 MethodMatcher 이다. MethodMatcher 인터페이스를 구현한다.
+     * matches() :
+     *  이 메서드에 method , targetClass 정보가 넘어온다.
+     *  이 정보로 어드바이스를 적용할지 적용하지 않을지 판단할 수 있다.
+     *  여기서는 메서드 이름이 "save" 인 경우에 true 를 반환하도록 판단 로직을 적용했다.
+     *
+     * isRuntime() , matches(... args) :
+     *  isRuntime() 이 값이 참이면 matches(... args) 메서드가 대신 호출된다.
+     *  동적으로 넘어오는 매개변수를 판단 로직으로 사용할 수 있다.
+     *  isRuntime() 이 false 인 경우 클래스의 정적 정보만 사용하기 때문에 스프링이 내부에서 캐싱을 통해 성능 향상이 가능하지만, isRuntime() 이 true 인 경우 매개변수가 동적으로 변경된다고 가정하기 때문에 캐싱을 하지 않는다.
+     */
+    @Slf4j
+    static class MyMethodMatcher implements MethodMatcher {
+        private static final String matchName = "save";
+
+        /* isRuntime false; 클래스 정적 정보만 사용(caching 사용 가능)*/
+        @Override
+        public boolean matches(Method method, Class<?> targetClass) {//파라미터가 정적으로 넘어온다.
+            boolean result = method.getName().equals(matchName);
+            log.info("포인트컷 호출 method={}, targetClass={}", method.getName(), targetClass);
+            log.info("포인트컷 결과={}", result);
+            return result;
+        }
+
+        @Override
+        public boolean isRuntime() {
+            return false;
+        }
+
+        /* isRuntime true; 매개변수(파라미터) 동적 변경 (caching 사용 불가)*/
+        @Override
+        public boolean matches(Method method, Class<?> targetClass, Object... args) {//args 가 넘어오는 것은 caching 이 안됨. 동적할당이기 때문..
+            return false;
+        }
     }
 }
